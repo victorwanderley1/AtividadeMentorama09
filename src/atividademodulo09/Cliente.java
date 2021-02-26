389074ddcbceb1e6203fc1c73a75423a34b30862
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-02-25 22:52:57 -0300

    Adicionando métodos iniciais à Classe Cliente

--- src/atividademodulo09/Cliente.java
+++ src/atividademodulo09/Cliente.java
@@ -7,9 +7,48 @@ import java.util.List;
  *
  * @author Victor Wanderley
  */
-public class Cliente {
+public class Cliente implements ComprasCliente{
     private String nome;
     List<Double> compras = new ArrayList();
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
     
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Double> getCompras() {
+        return compras;
+    }
+
+    public void setCompras(List<Double> compras) {
+        this.compras = compras;
+    }
     
+    /**
+     * Função recebe valor da compra e efetua a compra adicionando na
+     * lista dos valores das compras feitas pelo cliente.
+     * @param valorCompra
+     */
+    public void fazerCompra(double valorCompra){
+        this.compras.add(valorCompra);
+    }
+    
+    /**
+     * A função obtem a lista dos valores das compras feitas pelo cliente
+     * e retorna o valor total das compras.
+     * @return
+     */
+    @Override
+    public double totalCompras() {
+        double valorTotal = 0;
+        valorTotal = this.getCompras().stream().map(valor -> valor).reduce(valorTotal,(valor, acumulador) -> acumulador+valor);
+        return valorTotal;
+    }
 }
