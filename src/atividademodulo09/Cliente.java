8acf40c117260d84c563a983cc4973f995ed05e7
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-02-25 21:28:43 -0300

    Iniciando Classe Cliente

---
+++ src/atividademodulo09/Cliente.java
@@ -0,0 +1,15 @@
+package atividademodulo09;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Victor Wanderley
+ */
+public class Cliente {
+    private String nome;
+    List<Double> compras = new ArrayList();
+    
+    
+}
