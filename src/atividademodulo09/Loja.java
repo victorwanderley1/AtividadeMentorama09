aca83c6433edfba98c369bc796eff61169032be9
Victor Wanderley <victor.wanderley1709@gmail.com>
2021-02-28 00:09:16 -0300

    Criando métodos iniciais

---
+++ src/atividademodulo09/Loja.java
@@ -0,0 +1,43 @@
+package atividademodulo09;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Victor Wanderley
+ */
+public class Loja{
+    private List<Cliente> listaDeClientes = new ArrayList();
+
+    public List<Cliente> getListaDeClientes() {
+        return listaDeClientes;
+    }
+
+    public void setListaDeClientes(List<Cliente> listaDeClientes) {
+        this.listaDeClientes = listaDeClientes;
+    }
+    
+    public void adicionarCliente(String nomeCliente){
+        this.listaDeClientes.add(new Cliente(nomeCliente));
+        Collections.sort(this.listaDeClientes);
+    }
+    
+    public void adicionarCliente(String[] nome){
+        for(String nomeCliente: nome){
+            this.listaDeClientes.add(new Cliente(nomeCliente));
+        }
+        Collections.sort(this.listaDeClientes);
+    }
+    
+    public Cliente clienteComMaiorCompra(){
+        return this.listaDeClientes.stream().max(new OrdemValorCompras()).get();
+    }
+    
+    public Cliente clienteComMenorCompra(){
+        return this.listaDeClientes.stream().min(new OrdemValorCompras()).get();
+    }
+    
+    
+}
